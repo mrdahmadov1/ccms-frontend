edb6b1a8226aa4c2423cbc44724fc63fea6d8268
DESKTOP-KTIOEGP\Murad Ahmadov <murad.ahmadov@vabiss.com>
2024-07-06 14:17:44 +0400

    - apply lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,98 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AuthChecker from './pages/authChecker';
-import Signin from './pages/auth/signin';
-import Signup from './pages/auth/signup';
-import Dashboard from './pages/main/dashboard';
-import SendComplaint from './pages/main/sendComplaint';
-import NotFoundPage from '../404';
+import { lazy, Suspense } from 'react';
 import { Provider } from 'react-redux';
 import store from './store/configureStore';
 import { ToastContainer } from 'react-toastify';
-import MyComplaints from './pages/main/myComplaints';
-import Admin from './pages/main/admin';
-import AllComplaints from './pages/main/allComplaints';
-import ComplaintDetail from './pages/main/complaintDetail';
+import NotFoundPage from '../404';
+import { Box, LinearProgress } from '@mui/material';
+
+const Signin = lazy(() => import('./pages/auth/signin'));
+const Signup = lazy(() => import('./pages/auth/signup'));
+const Dashboard = lazy(() => import('./pages/main/dashboard'));
+const SendComplaint = lazy(() => import('./pages/main/sendComplaint'));
+const MyComplaints = lazy(() => import('./pages/main/myComplaints'));
+const ComplaintDetail = lazy(() => import('./pages/main/complaintDetail'));
+const Admin = lazy(() => import('./pages/main/admin'));
+const AllComplaints = lazy(() => import('./pages/main/allComplaints'));
+const AuthChecker = lazy(() => import('./pages/authChecker'));
 
 const App = () => {
   return (
     <>
       <Provider store={store}>
         <Router>
-          <Routes>
-            <Route path="/signin" element={<Signin />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route
-              path="/"
-              element={
-                <AuthChecker>
-                  <Dashboard />
-                </AuthChecker>
-              }
-            />
-            <Route
-              path="/send-complaint"
-              element={
-                <AuthChecker>
-                  <SendComplaint />
-                </AuthChecker>
-              }
-            />
-            <Route
-              path="/my-complaints"
-              element={
-                <AuthChecker>
-                  <MyComplaints />
-                </AuthChecker>
-              }
-            />
-            <Route
-              path="/my-complaints/:id"
-              element={
-                <AuthChecker>
-                  <ComplaintDetail />
-                </AuthChecker>
-              }
-            />
-            <Route
-              path="/admin"
-              element={
-                <AuthChecker>
-                  <Admin />
-                </AuthChecker>
-              }
-            />
-            <Route
-              path="/admin/all-complaints"
-              element={
-                <AuthChecker>
-                  <AllComplaints />
-                </AuthChecker>
-              }
-            />
-            <Route
-              path="/admin/all-complaints/:id"
-              element={
-                <AuthChecker>
-                  <ComplaintDetail />
-                </AuthChecker>
-              }
-            />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
+          <Suspense
+            fallback={
+              <Box sx={{ width: '100%' }}>
+                <LinearProgress />
+              </Box>
+            }
+          >
+            <Routes>
+              <Route path="/signin" element={<Signin />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route
+                path="/"
+                element={
+                  <AuthChecker>
+                    <Dashboard />
+                  </AuthChecker>
+                }
+              />
+              <Route
+                path="/send-complaint"
+                element={
+                  <AuthChecker>
+                    <SendComplaint />
+                  </AuthChecker>
+                }
+              />
+              <Route
+                path="/my-complaints"
+                element={
+                  <AuthChecker>
+                    <MyComplaints />
+                  </AuthChecker>
+                }
+              />
+              <Route
+                path="/my-complaints/:id"
+                element={
+                  <AuthChecker>
+                    <ComplaintDetail />
+                  </AuthChecker>
+                }
+              />
+              <Route
+                path="/admin"
+                element={
+                  <AuthChecker>
+                    <Admin />
+                  </AuthChecker>
+                }
+              />
+              <Route
+                path="/admin/all-complaints"
+                element={
+                  <AuthChecker>
+                    <AllComplaints />
+                  </AuthChecker>
+                }
+              />
+              <Route
+                path="/admin/all-complaints/:id"
+                element={
+                  <AuthChecker>
+                    <ComplaintDetail />
+                  </AuthChecker>
+                }
+              />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </Suspense>
         </Router>
       </Provider>
-      <ToastContainer newestOnTop autoClose={1500} position="top-center" />
+      <ToastContainer newestOnTop autoClose={1000} position="top-center" />
     </>
   );
 };
