ca7fc8528627939d428862dfbde4610655411f20
DESKTOP-KTIOEGP\Murad Ahmadov <murad.ahmadov@vabiss.com>
2024-07-07 06:54:44 +0400

    - checkLogin manually visited authPages

--- src/pages/auth/signin/index.jsx
+++ src/pages/auth/signin/index.jsx
@@ -1,7 +1,23 @@
 import SignInForm from '../../../components/auth/signinForm';
 import AuthLayout from '../../../layouts/authLayout';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { checkLogin } from '../../../store/userSlice';
 
 function SignIn() {
+  const dispatch = useDispatch();
+  const { user, isLoggedIn } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(checkLogin());
+    if (user && isLoggedIn) {
+      if (user.role === 'admin') navigate('/admin');
+      else if (user.role === 'user') navigate('/');
+    }
+  }, [isLoggedIn, user, navigate, dispatch]);
+
   return (
     <>
       <AuthLayout>
