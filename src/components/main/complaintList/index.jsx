ebd39f2bd63dd34dbebd8916495970285772595b
DESKTOP-KTIOEGP\Murad Ahmadov <murad.ahmadov@vabiss.com>
2024-07-08 14:12:11 +0400

    - add pagination to complaint list

--- src/components/main/complaintList/index.jsx
+++ src/components/main/complaintList/index.jsx
@@ -3,7 +3,7 @@ import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import IconButton from '@mui/material/IconButton';
 import ReadMoreIcon from '@mui/icons-material/ReadMore';
-import { Grid, Container } from '@mui/material';
+import { Grid, Container, Box, Pagination } from '@mui/material';
 import { useLocation, useNavigate } from 'react-router-dom';
 import ComplaintSearchFilterBar from '../complaintFilterBar';
 import { useEffect, useState } from 'react';
@@ -12,7 +12,8 @@ const ComplaintList = ({ complaints }) => {
   const location = useLocation();
   const navigate = useNavigate();
   const [filteredComplaints, setFilteredComplaints] = useState([]);
-
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
   useEffect(() => {
     setFilteredComplaints(complaints);
   }, [complaints]);
@@ -26,11 +27,21 @@ const ComplaintList = ({ complaints }) => {
     setFilteredComplaints(filtered);
   };
 
+  // Calculate current items based on pagination
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredComplaints.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Handle page change
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
+  };
+
   return (
     <Container>
       <ComplaintSearchFilterBar onSearch={handleSearch} />
       <Grid container spacing={1}>
-        {filteredComplaints.map(({ _id, title, submissionDate }) => (
+        {currentItems.map(({ _id, title, submissionDate }) => (
           <Grid item xs={12} key={_id}>
             <Card sx={{ paddingRight: 2, maxWidth: '100%', boxShadow: 1 }}>
               <CardHeader
@@ -58,6 +69,16 @@ const ComplaintList = ({ complaints }) => {
           </Grid>
         ))}
       </Grid>
+      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+        <Pagination
+          count={Math.ceil(filteredComplaints.length / itemsPerPage)}
+          page={currentPage}
+          color="primary"
+          boundaryCount={0}
+          onChange={handlePageChange}
+          sx={{ mt: 2, justifyContent: 'center' }}
+        />
+      </Box>
     </Container>
   );
 };
